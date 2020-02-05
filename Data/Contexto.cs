6dd556485124f21100770717f9aa5a4d96b388b1
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2020-02-04 23:29:03 -0400

    Creando la cadena de conexiom e instalando los paquetes de EntityFrameWorkCore

---
+++ Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcialJuanFcoR_2017_0039.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SqlExpress;Database=PrimerParcialDb;Trusted_Connection=True;");
+        }
+    }
+}
